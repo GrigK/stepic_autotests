70c9ecb2fba47b840e02f138d9aabce62aa5762d
FrendsTeamApp <FrendsTeamApp@gmail.com>
2019-10-03 21:46:18 +0300

    learn pytest.mark.parametrize

--- test_fixture7.py
+++ test_fixture7.py
@@ -52,3 +52,47 @@ def test_guest_should_see_login_link(browser, code, lang)
     link = "http://selenium1py.pythonanywhere.com/{}/".format(code)
     print("Проверяемый язык %s" % lang)
 """
+
+"""
+import pytest
+
+languages = ["ru", "de", "ua", "en-gb",]
+labels = ["russian", "german", "ukrainian", "english", ]
+
+@pytest.mark.parametrize("code", languages, ids=labels)
+def test_guest_should_see_login_link(code):
+    link = "http://selenium1py.pythonanywhere.com/{}/".format(code)
+ 
+
+Но у него есть подводный камень - из коробки pytest ескейпирует значения ids и если использовать там кириллицу 
+или что-то не ascii, то получится фигня. Лечится переопределением pytest_make_parametrize_id в conftest.
+"""
+
+"""
+еще вариант:
+
+import pytest
+from selenium import webdriver
+
+@pytest.fixture(scope="function")
+def dr():
+    print("\nstart dr for test..")
+    dr = webdriver.Chrome()
+    yield dr
+    print("\nquit dr..")
+    dr.quit()
+    
+languages = [
+    ("ru", "русский"),
+    ("de", "немецкий"),
+    pytest.param("ua", "украинский",  marks=pytest.mark.xfail(reason="no ua language")),
+    ("en-gb", "английский")
+]
+
+@pytest.mark.parametrize("code, lang", languages)
+def test_guest_should_see_login_link(dr, code, lang): 
+    link = "http://selenium1py.pythonanywhere.com/{}/".format(code)
+    print("Проверяемый язык %s" % lang)
+    dr.get(link)
+    dr.find_element_by_css_selector("#login_link")
+"""
